5af7328754eb40414f4305482e4e5a5dceabe54e
Aleksey Nesterov <zubatka.forever@icloud.com>
2025-05-18 18:31:44 +0400

    +

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"url_shortener/internal/config"
+)
+
+const (
+	envProd  = "prod"
+	envLocal = "local"
+	envDev   = "dev"
+)
+
+func main() {
+
+	// os.Setenv("CONFIG_PATH", "./config/local.yaml")
+	// fmt.Println("get pathvar: ", os.Getenv("CONFIG_PATH"))
+
+	// TODO: init config - cleanenv
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+	log.Info("Starting app", slog.String("env", cfg.Env))
+
+	log.Debug("Debug messages are enabled")
+
+	//TODO: init logger - slog
+
+	//TODO: init storage
+
+	//TODO: init router - chi, "chi render"
+
+	//TODO: run server
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+
+}
